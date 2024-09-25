47f339221d8966a3ffbb911c5e04300e4c37751f
MoseyQAQ <admin@aleeqaq.cc>
2024-09-25 20:47:41 +0800

    update

--- ferrodispcalc/type_map.py
+++ ferrodispcalc/type_map.py
@@ -11,3 +11,5 @@ UniPero: list[str] = ['Ba','Pb','Ca','Sr','Bi',
             'K','Na','Hf','Ti','Zr',
             'Nb','Mg','In','Zn','O']
 PSTO: list[str]=['Sr','Pb','Ti','O']
+
+GaN: list[str] = ['N', 'Sc', 'Ga']
