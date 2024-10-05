f056c889f21af65a41b803c4f304492dbf57aff1
MoseyQAQ <admin@aleeqaq.cc>
2024-10-05 21:10:46 +0800

    update get_p

--- 1.sh
+++ 1.sh
@@ -1,1 +1,2 @@
 export PYTHONPATH=/calc_cache/lidenan/develop/FerroDispCalc:$PYTHONPATH
+export CPATH=/shared_storage/home/lidenan/code/eigen-3.4.0/:$CPATH
--- src/binding.cpp
+++ src/binding.cpp
@@ -2,11 +2,14 @@
 #include <pybind11/eigen.h>
 #include <pybind11/stl.h>
 #include "get_averaged_structure.hpp"
-
+#include "get_displacement.hpp"
+#include "get_polarization.hpp"
 namespace py = pybind11;
 
 PYBIND11_MODULE(fdc, m) {
     m.doc() = "Averaging the structure of a trajectory";
 
     m.def("get_averaged_structure", &get_averaged_structure, "A function to average the structure of a trajectory");
+    m.def("get_displacement", &get_displacement, "A function to calculate the displacement of a trajectory");
+    m.def("get_polarization", &get_polarization, "A function to calculate the polarization of a trajectory");
 }
