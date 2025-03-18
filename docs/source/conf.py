8bec827bd15202cc704e2c3139bbcd7481045c5e
MoseyQAQ <admin@aleeqaq.cc>
2025-03-18 15:44:43 +0800

    init dcs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,36 +7,24 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'FerroDispCalc'
-copyright = '2024, Denan LI'
+copyright = '2025, Denan LI'
 author = 'Denan LI'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-
-# -- Path setup --------------------------------------------------------------
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../ferrodispcalc'))
---
+++ docs/source/getting-started/index.rst
@@ -0,0 +1,11 @@
+Getting Started
+===============
+
+In this text, we will call the deep neural network that is used to represent the interatomic interactions (Deep Potential) the model. The typical procedure of using DeePMD-kit is
+
+.. toctree::
+   :maxdepth: 2
+   :numbered:
+
+   install
+   quick_start
